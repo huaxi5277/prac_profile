7b91a922dc67e05530655a8834e2d3fdbe2b1776
huaxi527 <xue19970326@qq.com>
2019-12-02 19:21:42 +0800

    acb

--- static/app.js
+++ static/app.js
@@ -1,7 +1,17 @@
 var express = require("express")
+var app = express()
 var router = express.Router()
 express.request(router)
 
 router.use("/",(rea,res)=>{
     res.send(JSON.stringify("1111"))
 })
+
+app.listen(3000,(err)=>{
+    if(err){
+        console.log(err)
+        return 
+    }
+    console.log("server start")
+})
+
